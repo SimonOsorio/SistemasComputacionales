d1e77f9c7a7e20dea8c12a8cc4b8c5b65bad3c9b
SimonOsorio <simonosoriouribe@gmail.com>
2022-02-17 10:38:44 -0500

    Creacion Repositorio y Primer ejercicio en C

---
+++ C/1 - Testing/main.c
@@ -0,0 +1,10 @@
+//Para compilar en la consola se utiliza gcc -Wall -g main.c -o main
+//Y para ejecutar se utiliza ./main
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    printf("Sistemas computacionales\n");
+    return EXIT_SUCCESS;
+}
